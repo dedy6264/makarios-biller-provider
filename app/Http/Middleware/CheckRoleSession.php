6bc95441fec6375d0bdeffd363e29354333cd851
dedy.kusworo <dedy.kusworo@gmail.com>
2026-03-31 19:54:05 +0700

    update session

--- app/Http/Middleware/CheckRoleSession.php
+++ app/Http/Middleware/CheckRoleSession.php
@@ -29,6 +29,7 @@ class CheckRoleSession
             return redirect()->route('dashboard')->with('error', 'Anda tidak memiliki akses ke halaman tersebut.');
         }
         if(session('role_id')==3){
+            
             return redirect()->route('mini_apps.index');
         }
         return $next($request);
