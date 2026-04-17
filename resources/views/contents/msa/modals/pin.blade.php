0c0c514929cb4c10038b560e0bc41357149616e4
dedy.kusworo <dedy.kusworo@gmail.com>
2026-04-17 09:23:41 +0700

    update data

--- resources/views/contents/msa/modals/pin.blade.php
+++ resources/views/contents/msa/modals/pin.blade.php
@@ -1,6 +1,6 @@
 <div v-if=isModalPin>
     {{-- <main class="flex flex-col items-center flex-1 w-full max-w-md px-8 pt-24 pb-12"> --}}
-        <main class="max-w-md px-6 pb-32 mx-auto pt-50">
+        <main class="max-w-md px-6 pt-16 pb-32 mx-auto">
             <transition enter-active-class="transition duration-300 ease-out"
                 enter-from-class="transform -translate-y-2 opacity-0"
                 enter-to-class="transform translate-y-0 opacity-100"
@@ -14,11 +14,19 @@
             </transition>
             <!-- Brand/Identity Lockup -->
             <div class="mb-10 text-center">
+                {{-- <div
+                    class="flex items-center justify-center w-16 h-16 mx-auto mb-6 bg-primary-container/10 rounded-2xl">
+                    <span class="text-4xl material-symbols-outlined text-primary-container" data-icon="lock"
+                        style="font-variation-settings: 'FILL' 1;">lock</span>
+                </div>
+                <h2 class="mb-3 text-2xl font-extrabold tracking-tight text-on-surface">Enter your 6-digit transaction
+                    PIN
+                </h2> --}}
                 <p class="text-sm font-medium text-on-surface-variant">Please enter your secure PIN to authorize this
                     transfer.</p>
             </div>
             <!-- PIN Input Visualization -->
-            <div class="flex gap-3 mb-12">
+            <div class="flex gap-3 mb-6">
                 <div v-for="i in pinLimit" :key="i"
                     class="flex items-center justify-center w-12 h-12 transition-all duration-200 border-2 shadow-sm rounded-xl"
                     :class="pin.length >= i 
@@ -31,7 +39,7 @@
                 </div>
             </div>
             <!-- Numeric Keypad -->
-            <div class="grid grid-cols-3 gap-6 w-full max-w-[320px] mb-10">
+            <div class="grid grid-cols-3 gap-6 w-full max-w-[320px] mb-6">
                 <button v-for="n in [1,2,3,4,5,6,7,8,9]" :key="n" @click="addNumber(n)"
                     class="aspect-square rounded-full bg-surface-container-lowest text-2xl font-headline font-bold text-on-surface flex items-center justify-center shadow-[0px_4px_12px_rgba(0,62,199,0.04)] active:scale-90 transition-transform duration-150">
                     @{{ n }}
@@ -54,11 +62,6 @@
             </button>
             <!-- Primary Action -->
             <div class="fixed bottom-0 left-0 z-50 w-full px-6 pt-4 pb-10 bg-white/70 backdrop-blur-xl">
-                <button v-else @click="closeModals"
-                    class="flex items-center justify-center flex-1 gap-2 px-4 py-4 font-bold transition-all bg-red-600 text-on-primary font-label rounded-2xl active:scale-95">
-                    <span class="text-xl text-white material-symbols-outlined">cancel</span>
-                    Close
-                </button>
                 <button @click="confirmPin" :disabled="pin.length !== pinLimit"
                     class="w-full h-14 rounded-2xl font-headline z-50 font-bold text-lg shadow-[0px_12px_32px_rgba(0,62,199,0.15)] active:scale-[0.98] transition-all duration-300"
                     :class="pin.length === pinLimit 
